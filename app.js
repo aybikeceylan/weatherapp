5003686cc855862740120146b275aa8c9a14f8a8
aybike <aybikesayar@gmail.com>
2022-09-26 09:34:59 +0300

    finish

--- app.js
+++ app.js
@@ -2,20 +2,28 @@ const search = document.querySelector(".search")
 const btn = document.querySelector(".btn")
 const main = document.querySelector(".main")
 
-const fetchCity = (name) => {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=98ee7a2b10fb4e81ffcc70e6a64d53c3`
+let counter = 0;
 
-    fetch(url)
-        .then((res) => {
-            if (!res.ok) {
-                console.log(`Something went wrong: ${res.status}`);
-                renderError();
-                throw new Error();
-            }
-            return res.json();
-        })
-        .then((data) => renderCity(data))
-        .catch((err) => console.log(err))
+const fetchCity = (name) => {
+    if (counter > 3) {
+        alert("maximum arama sınırına ulaştınız")
+    }
+    else {
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=98ee7a2b10fb4e81ffcc70e6a64d53c3`
+
+        fetch(url)
+            .then((res) => {
+                if (!res.ok) {
+                    console.log(`Something went wrong: ${res.status}`);
+                    renderError();
+                    throw new Error();
+                }
+                return res.json();
+            })
+            .then((data) => renderCity(data))
+            .catch((err) => console.log(err))
+
+    }
 }
 
 const renderCity = (data) => {
@@ -24,20 +32,25 @@ const renderCity = (data) => {
 
     console.log(name);
     console.log(Object.values(weather)[0].description)
+    console.log(Object.values(weather)[0].icon)
     console.log(temp);
     console.log(humidity);
     console.log(speed);
 
     main.innerHTML += `
     <div class="card">
-      <div class="card-text">
+      <div class="card-text card-xmark">
         <p class="name">Weather in ${name}</p>
+        <i class="fa-solid fa-xmark"></i>
       </div>
       <div class="card-text">
         <p class="temp">${Math.floor(temp) - 273}°C </p>
       </div>
       <div class="card-text">
-        <p class="status">${Object.values(weather)[0].description}</p>
+            <img class="icon" src="http://openweathermap.org/img/wn/${Object.values(weather)[0].icon}.png" alt="">
+            <p>${Object.values(weather)[0].description}</p>
+        </div>
+      <div class="card-text">
         <p class="status">Humidity:% ${humidity}</p>
         <p class="status">Wind Speed:${speed}</p>
       </div>
@@ -55,6 +68,16 @@ const renderError = () => {
 btn.addEventListener("click", () => {
     input = search.value
     fetchCity(input)
+    search.value = ""
+    console.log(input)
+    counter++
+})
+main.addEventListener("click", (e) => {
+
+    console.log(e.target)
+    if (e.target.className == "fa-solid fa-xmark") {
+        e.target.parentElement.parentElement.remove()
+    }
 })
 
 
